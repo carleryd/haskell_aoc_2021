c2be9552590424351d8ebed0758116f4bf7c8335
Christofer Ärleryd <c.arleryd@gmail.com>
2021-12-23 09:21:58 +1300

    Solved day 4 part 1 but solution needs improving

---
+++ day4/part1.hs
@@ -0,0 +1,81 @@
+import Data.List
+import Data.Maybe
+
+type BoardRow = [Int]
+type Board = [BoardRow]
+type Line = String
+type BingoNumbers = [Int]
+
+splitComma :: String -> [String]
+splitComma s =  case dropWhile (==',') s of
+             "" -> []
+             s' -> w : splitComma s''
+               where (w, s'') = break (==',') s'
+
+constructBoards :: [Line] -> [Board]
+constructBoards [] = []
+constructBoards xs =
+  let
+    xs' = dropWhile (=="") xs
+    (start, rest) = splitAt 5 xs'
+    board =  fmap (\line -> fmap (\word -> read word :: Int) $ words line) start
+  in
+    [board] ++ constructBoards rest
+
+parseInput :: [String] -> (BingoNumbers, [Board])
+parseInput xs =
+  let
+    bingoNumbers = fmap (\word -> read word :: Int) $ splitComma $ head xs
+    boards = constructBoards $ tail xs
+  in
+    (bingoNumbers, boards)
+
+getBingoScore :: [String] -> Maybe Int
+getBingoScore xs =
+  let
+    (bingoNumbers, boards) = parseInput xs
+    (_, scoreM) = foldr (\board (prevDrawn, prevM) ->
+           let
+             (currDrawn, currM) = getScore 0 board bingoNumbers
+           in
+             if (currDrawn < prevDrawn && isJust currM) then (currDrawn, currM) else (prevDrawn, prevM)
+          ) (9999, Nothing) boards
+  in
+    scoreM
+
+getBoardScore :: Board -> BingoNumbers -> Maybe Int
+getBoardScore board bingoNumbers =
+  let
+    checkBingo :: [Int] -> Bool
+    checkBingo xs = foldr (\num acc -> acc && elem num bingoNumbers) True xs
+
+    hasRow = foldr (\line acc -> acc || checkBingo line) False board
+    hasColumn = foldr (\line acc -> acc || checkBingo line) False $ transpose board
+
+    score = foldr (\number acc -> case elem number bingoNumbers of
+                         True -> acc
+                         False -> acc + number
+                     ) 0 $ concat board
+  in
+    case hasRow || hasColumn of
+      True -> Just score
+      False -> Nothing
+
+getScore :: Int -> Board -> BingoNumbers -> (Int, Maybe Int)
+getScore i board bingoNumbers =
+  let
+    partial = take (i + 1) bingoNumbers
+    scoreM = getBoardScore board partial
+
+    continue = (i + 1) < length bingoNumbers && isNothing scoreM
+    lastNumber = bingoNumbers !! i
+  in
+    case continue of
+      False -> (i, fmap (\score -> score * lastNumber) scoreM)
+      True -> getScore (i + 1) board bingoNumbers
+
+main :: IO ()
+main = do
+  contents <- readFile "input.txt"
+
+  print $ getBingoScore $ lines contents
